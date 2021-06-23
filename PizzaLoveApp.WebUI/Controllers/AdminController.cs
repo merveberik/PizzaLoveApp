306d26c5f78fb6c6a124a4ef07b7610b1f67d219
Emre Demir <emre2548@gmail.com>
2021-06-23 22:17:48 +0300

    Added add new product page

--- PizzaLoveApp.WebUI/Controllers/AdminController.cs
+++ PizzaLoveApp.WebUI/Controllers/AdminController.cs
@@ -3,13 +3,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PizzaLoveApp.Business.Abstract;
+using PizzaLoveApp.Entities;
+using PizzaLoveApp.WebUI.Models;
 
 namespace PizzaLoveApp.WebUI.Controllers
 {
     public class AdminController : Controller
     {
+        private IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View(new ProductModel());
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductModel product)
+        {
+            var entity = new Product()
+            {
+                Name = product.Name,
+                ImageUrl = product.ImageUrl,
+                Price = product.Price,
+                Description = product.Description,
+                Size = product.Size
+            };
+
+            _productService.Create(entity);
+
             return View();
         }
     }
