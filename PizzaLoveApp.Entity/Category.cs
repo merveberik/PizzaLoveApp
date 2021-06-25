37a1758c58efce05da66d55f2943545c223a81f3
Emre Demir <emre2548@gmail.com>
2021-06-25 23:37:43 +0300

    Added category icon imageurl address in the DB

--- PizzaLoveApp.Entity/Category.cs
+++ PizzaLoveApp.Entity/Category.cs
@@ -10,6 +10,7 @@ namespace PizzaLoveApp.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ImageUrl { get; set; }
 
         public List<ProductCategory> ProductCategories { get; set; }
     }
