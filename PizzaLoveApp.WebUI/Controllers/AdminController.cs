cf72be636262cdba9bb75f6b8d70bdb4da891712
merve <merve@DESKTOP-6GM9IIA>
2021-06-25 03:37:16 +0300

    Added Edit and fixed layers

--- PizzaLoveApp.WebUI/Controllers/AdminController.cs
+++ PizzaLoveApp.WebUI/Controllers/AdminController.cs
@@ -60,7 +60,7 @@ namespace PizzaLoveApp.WebUI.Controllers
 
             if (entity == null)
             {
-                return NotFound();
+                return NotFound(); 
             }
             var model = new ProductModel()
             {
