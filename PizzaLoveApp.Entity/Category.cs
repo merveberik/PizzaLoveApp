dc5d6e986091b862d51a7867845a7a69309b04ce
merve <merve@DESKTOP-6GM9IIA>
2021-07-01 23:26:45 +0300

    null commit

--- PizzaLoveApp.Entity/Category.cs
+++ PizzaLoveApp.Entity/Category.cs
@@ -11,7 +11,6 @@ namespace PizzaLoveApp.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
-
         public List<ProductCategory> ProductCategories { get; set; }
     }
 }
